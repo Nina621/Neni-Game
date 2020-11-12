8ac14229c881cd2cdae4a6136eb26b2698865f49
Nikolina Marinić <61114828+Nina621@users.noreply.github.com>
2020-11-12 15:42:58 +0100

    Add files via upload

---
+++ CoinSound.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinSound : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    private AudioSource audioSource;
+    private BoxCollider2D boxCollider2D;
+    private bool ReadyToDestroy;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        audioSource = GetComponent<AudioSource>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        if (ReadyToDestroy == true && audioSource.isPlaying == false)
+        {
+            Destroy(this);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Nensi")
+        {
+            ReadyToDestroy = true;
+            audioSource.Play();
+            spriteRenderer.enabled = false;
+            boxCollider2D.enabled = false;
+        }
+    }
+}
